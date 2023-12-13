e2848d75a5991d9faba2040c6267536b3200a3e1
GaneshChintalapudi <113678047+GaneshChintalapudi@users.noreply.github.com>
2023-12-13 14:03:54 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("localhost","root","","nipahtmsdb");
+$con=mysqli_connect("nivtms.mysql.database.azure.com","nivtms","Password@1234","nipahtmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
